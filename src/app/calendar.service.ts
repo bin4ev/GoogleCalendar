a2dd7f357ea90f60b2c1fda8318deac9c9cac994
Georgi Binchev <bin4ev@gmail.com>
2022-03-20 22:49:06 +0200

    add dayView events cascade

--- src/app/calendar.service.ts
+++ src/app/calendar.service.ts
@@ -13,51 +13,77 @@ export class CalendarService {
       {
         id: '1',
         name: 'Busy',
-        start: '9:20 AM',
-        end: '10:50 AM',
-        date: '14/03',
+        start: '7:20 AM',
+        end: '11:50 AM',
+        date: '17/03',
+        color: 'red'
+      },
+      {
+        id: '0',
+        name: 'Meet with Ivan',
+        start: '8:20 AM',
+        end: '12:20 AM',
+        date: '17/03',
         color: 'red'
       },
       {
         id: '2',
         name: 'Dinner',
-        start: '12:20 PM',
-        end: '14:00 AM',
-        date: '14/03',
+        start: '3:20 AM',
+        end: '6:00 AM',
+        date: '17/03',
         color: 'red'
       },
       {
         id: '3',
         name: 'Meeting Bussines',
-        start: '3:50 AM',
-        end: '6:00 AM',
-        date: '15/03',
+        start: '3:10 AM',
+        end: '5:30 AM',
+        date: '17/03',
         color: 'red'
       },
       {
         id: '4',
         name: 'Dentis ',
-        start: '2:20 AM',
-        end: '5:00 AM',
-        date: '14/03',
+        start: '3:20 AM',
+        end: '4:15 PM',
+        date: '17/03',
         color: 'red'
       }
+    ],
+    Stamat: [
+      {
+        id: '8',
+        name: 'Interview',
+        start: '3:10 AM',
+        end: '9:30 AM',
+        date: '17/03',
+        color: 'blue'
+      },
+      {
+        id: '9',
+        name: 'Doctor',
+        start: '8:00 AM',
+        end: '8:50 AM',
+        date: '17/03',
+        color: 'blue'
+      },
     ],
     Dragan: [
       {
         id: '6',
         name: 'Doctor ',
         start: '4:50 AM',
-        end: '6:00 AM',
-        date: '15/03',
+        end: '12:00 AM',
+        date: '17/03',
         color: 'green'
       },
       {
         id: '7',
         name: 'JavaScript Course',
-        start: '9:00 AM',
-        end: '10:50 AM',
-        date: '14/03',
+        start: '8:00 AM',
+        end: '8:50 AM',
+        date: '17/03',
         color: 'green'
       }
     ],
@@ -117,6 +143,7 @@ export class CalendarService {
     return {
       hour: h + format,
       min: m,
+      format
     }
   }
 }
--- src/app/day-calendar/day-calendar.component.ts
+++ src/app/day-calendar/day-calendar.component.ts
@@ -26,7 +26,7 @@ export class DayCalendarComponent {
     { id: 9, time: '9AM' },
     { id: 10, time: '10AM' },
     { id: 11, time: '11AM' },
-    { id: 12, time: '12PM' },
+    { id: 12, time: '12AM' },
     { id: 13, time: '1PM' },
     { id: 14, time: '2PM' },
     { id: 15, time: '3PM' },
@@ -58,40 +58,75 @@ export class DayCalendarComponent {
   getAllCalendars() {
     this.allCalendars = this.calendarService.getEventsForToday(this.showCalendarOf, this.currDay)
     this.parseDurationEvent()
-    this.setWithEvents()
+    this.setEvents()
   }
 
   parseDurationEvent() {
     for (let event of this.allCalendars) {
-      let minutes = (parseInt(event.endParse.hour) - parseInt(event.startParse.hour)) * 60
+      let timeStart = event.startParse.format == 'PM' ? parseInt(event.startParse.hour) + 12 : parseInt(event.startParse.hour)
+      let timeEnd = event.endParse.format == 'PM' ? parseInt(event.endParse.hour) + 12 : parseInt(event.endParse.hour)
+      let minutes = (timeEnd - timeStart) * 60
       minutes += event.endParse.min - event.startParse.min
       event.duration = minutes
+  }
+}
+
+setEvents() {
+  this.allCalendars.sort((a: any, b: any) => b.duration - a.duration)
+  for (let i = 1; i <= this.allCalendars.length; i++) {
+    this.allCalendars[i].zIndex = i + 1
+    this.allCalendars[i].left =  (i * 10) + '%'
+    this.allCalendars[i].width = 100 - (i * 10) + '%'
+  }
+
+/*   for (let event of this.allCalendars) {
+    for (let i = 0; i < this.allCalendars.length; i++) {
+      let x = this.allCalendars[i]
+
+      if (parseInt(x.start) > parseInt(event.start) &&
+        parseInt(event.end) > parseInt(x.start)) {
+        x.width = 100 - (i + 1 * 20) + '%'
+        x.left = (i+1) * 2 + '%'
+        continue
+      }
+
+      if (parseInt(x.start) < parseInt(event.start) &&
+        parseInt(event.end) < parseInt(x.start)) {
+        x.width = 100 - (i + 2 * 20) + '%'
+        x.left = (i+1) * 4 + '%'
+        continue
+      }
+
+      if (parseInt(x.start) < parseInt(event.start) &&
+      parseInt(event.end) < parseInt(x.start)) {
+       x.width = 100 - (i + 3* 20) + '%' 
+      x.left = (i+1) * 6 + '%'
+      continue
     }
-  }
 
-  setWithEvents() {
-    for (let event of this.allCalendars) {
-      let e = this.allCalendars.find((x: any) => event.end[0] > x.start[0])
-      event.width = '67%'
-      event.zIndex = 2
-      e.width = '97%'
-      e.zIndex = 1
+
+
+      if (parseInt(x.start) < parseInt(event.start) &&
+        parseInt(event.start) > parseInt(x.end)) {
+        x.width = 100 - (i + 4 * 20) + '%'
+        x.left = (i+1) * 7 + '%'
+      }
     }
-  }
+  } */
+}
 
-  setDateView(d: Date) {
-    this.currMonth = d.getMonth()
-    this.currDay = d
-    this.getAllCalendars()
+setDateView(d: Date) {
+  this.currMonth = d.getMonth()
+  this.currDay = d
+  this.getAllCalendars()
+}
 
-  }
-
-  isToday(date: Number) {
-    let d = new Date
-    return date == d.getDate() &&
-      this.currMonth == this.d.getMonth() &&
-      this.currYear == this.d.getFullYear()
-  }
+isToday(date: Number) {
+  let d = new Date
+  return date == d.getDate() &&
+    this.currMonth == this.d.getMonth() &&
+    this.currYear == this.d.getFullYear()
+}
 
 
 }
